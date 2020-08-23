21fc49c364cfc337b73d66a0f8e1f87cc0e07322
Fanan Dala <fanan.dala@yahoo.com>
2020-08-23 20:00:52 +0100

    completed stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,55 @@ void main(){
 }
 
 class MyFirstApp extends StatelessWidget{
+ 
+  @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(home: Text('Hello World!'),);
+    return MaterialApp(home: Home(),);
+  }
+}
+
+class Home extends StatefulWidget{
+  @override 
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int count = 0;
+  void increaseCount(){
+    setState(() {
+      this.count++;
+    });
+    
+    print(this.count);
+  }
+
+  void decreaseCount(){
+    setState(() {
+      this.count--;
+    });
+    
+    print(this.count);
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        title: Text("Akiddie"),
+      ),
+      body: Column(children: <Widget>[
+        Row(children: <Widget>[
+          RaisedButton(child: Text("Increase"), onPressed: increaseCount,), 
+          RaisedButton(child: Text("Decrease"), onPressed: decreaseCount,), 
+        ]),
+        Text('$count'),
+        ],
+      ),
+    ),
+    );
   }
 }
